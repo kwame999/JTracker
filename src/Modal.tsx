2f3cbee0353a69e7e37a75360ece1241f9d87472
Kiwi83 <Kwameasenso24@gmail.com>
2026-01-11 18:20:00 -0500

    Added labels and and proper handlers

--- src/Modal.tsx
+++ src/Modal.tsx
@@ -1,70 +1,5 @@
 import { useState } from "react"
 
-const Modal = () => {
-    
-    const [jobType, setJobType] = useState([{}])
-    console.log(jobType)
-    
-    function handleJobType(){
-        
-        setJobType(
-            [...jobType,  {
-                
-                id: "",
-                company: "",
-                role: "",
-                status: ["wishlist", "applied", "interview", "offer", "rejected", "ghosted"],
-                link: "",
-                createdAt: "",
-                rating: "",
-                moodTxt: "",
-                
-            }
-        ]
-        
-    )
-    
-        
-    }
-    
-    
-
-    return(
-    
-    <section>
-
-        <form action="">
-
-            <div>
-                <label htmlFor="">Company name:</label>
-                <input type="text" name="company" id="" />
-                <input type="text" name="role" id="" />
-                <input type="text" name="date" id="" />
-                <input type="text" name="link" id="" />
-            
-            </div>
-
-            <textarea name="moodMsg" id=""></textarea>
-            <select name="status" id="">
-                
-                <option value="wishlist">Wishlist</option>
-                <option value="applied">Applied</option>
-                <option value="interview">Interview</option>
-                <option value="offer">Offer</option>
-                <option value="rejected">Rejected</option>
-                <option value="ghosted">Ghosted</option>
-            
-            </select>
-
-            <button type="submit" onClick={handleJobType}>ss</button>
-        </form>
-
-    </section>
-)
-
-
-}
-
 type JobType = {
     
     id: number,
@@ -73,11 +8,103 @@ type JobType = {
     status: "wishlist" | "applied" | "interview" | "offer" | "rejected" | "ghosted",
     link?: string,
     createdAt: string,
-    rating: number,
+    rating?: number,
     moodTxt: string,
+
+}
+
+const Modal = () => {
+    
+    const [jobType, setJobType] = useState([{}])
+    const [company, setCompany] = useState("")
+    const [role, setRole] = useState("")
+    const [status, setStatus] = useState("")
+    const [link, setLink] = useState("")
+    const [createdAt, setCreatedAt] = useState("")
+    const [rating, setRating] = useState("-")
+    const [moodTxt, setMoodTxt] = useState("")
+
+    console.log(jobType)
+    
+    function handleJobType(){
+        
+        setJobType(
+            [...jobType,  {
+                
+                id: "",
+                company: company,
+                role: role,
+                status: status,
+                link: link,
+                createdAt: createdAt,
+                rating: rating,
+                moodTxt: moodTxt,
+                
+            }
+        ]
+        
+    )
+       
+    }
+    
+    
+
+    return(
+    
+        <section>
+
+            <form action="">
+
+                <div>
+                    <label htmlFor="">Company name:</label>
+                    <input type="text" name="company" id="" value={company} onChange={(e)=>{
+                        setCompany(e.target.value)
+                    }}/>
+                    <label htmlFor="">role:</label>
+                    <input type="text" name="role"  id="" value={role} onChange={(e)=>{
+                        setRole(e.target.value)
+                    }}/>
+                    <label htmlFor="">link</label>
+                    <input type="text" name="link" id="" value={link} onChange={(e)=>{
+                        setLink(e.target.value);
+                    }}/>
+                    <label htmlFor="created at"></label>
+                    <input type="text" name="date" id="" value={createdAt} onChange={(e)=>{
+                        setCreatedAt(e.target.value);
+                    }}/>
+                    <label htmlFor="mood"></label>
+                    <input type="text" name="mood" id="" value={moodTxt} onChange={(e)=>{
+                        setMoodTxt(e.target.value);
+                        
+                    }}/>
+
+                
+                </div>
+
+                <textarea name="moodMsg" id=""></textarea>
+                <select name="status" id="">
+                    
+                    <option value="wishlist">Wishlist</option>
+                    <option value="applied">Applied</option>
+                    <option value="interview">Interview</option>
+                    <option value="offer">Offer</option>
+                    <option value="rejected">Rejected</option>
+                    <option value="ghosted">Ghosted</option>
+                
+                </select>
+
+                <button type="submit" onClick={(e)=>{
+                    e.preventDefault();
+                    handleJobType()
+                }}>ss</button>
+            </form>
+
+        </section>
+)
 
 
 }
+
 
 const Card = (jobType: JobType) => {
     
