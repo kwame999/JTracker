4388cf2d48700b6b55aacddb0760c521c8229f6d
Kiwi83 <Kwameasenso24@gmail.com>
2026-01-17 19:28:42 -0500

    feat: Header tab, nav expanded

---
+++ src/Header.tsx
@@ -0,0 +1,20 @@
+type HeaderProps = {
+    jobProjName: string
+}
+const Header = ({jobProjName}: HeaderProps)=> {
+
+    return(
+        <section>
+            <div>
+                <img src="" alt="" />
+                <h1>{jobProjName}</h1>
+            <svg></svg>
+            </div>
+            <><img src="" alt="Gradient Mesh" /></>
+        </section>
+    )
+
+}
+
+
+export default Header
